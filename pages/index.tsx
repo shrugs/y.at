1e693d01952c59c0897e7303d1072df9ab28d93a
Matt Condon (shrugs) <mattgcondon@gmail.com>
2021-04-08 15:42:27 -0400

    feat: render homepage with origin pre-filled

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,27 @@
 import type { GetServerSideProps } from 'next';
+import { ComponentPropsWithoutRef } from 'react';
 
 import { getDestinationIfExists } from '../lib/db';
 
-function Home() {
-  return <div>hey</div>;
+function IndexPage({ origin }: { origin?: string }) {
+  return <div>hey. we're at {origin || 'apex'}</div>;
 }
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
+export const getServerSideProps: GetServerSideProps<
+  ComponentPropsWithoutRef<typeof IndexPage>
+> = async (ctx) => {
+  // we can trust the host url on vercel
   const host = ctx.req.headers.host;
 
-  // this is pretty naive, but does it really matter?
-  if (!host.includes('.')) {
-    return { notFound: true };
-  }
-
-  // this, too, but again, who cares?
+  // this is pretty naive, but who cares
   const origin = host.split('.')[0];
 
   // if we have a valid destination, redirect
   const destination = await getDestinationIfExists(origin);
   if (destination) return { redirect: { destination, permanent: true } };
 
-  // otherwise, render homepage
-  return { props: {} };
+  // otherwise, render homepage with pre-filled origin where applicable
+  return { props: { origin } };
 };
 
-export default Home;
+export default IndexPage;
